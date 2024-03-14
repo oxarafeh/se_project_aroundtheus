e6b5af95e101c90e72caacee39e102e3aced4b36
oxarafeh <oxarafeh@gmail.com>
2024-03-13 22:50:58 -0700

    added options to functions

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,62 +1,79 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+function showInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-};
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(errorClass);
+}
 
-const hideInputError = (formElement, inputElement) => {
+function hideInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
   errorElement.textContent = "";
-};
+}
 
-const checkInputValidity = (formElement, inputElement) => {
+function checkInputValidity(formElement, inputElement, options) {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, options);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, options);
   }
-};
+}
 
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
+function hasInputInvalid(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
 
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
+function toggleButtonState(inputElements, buttonElement) {
+  if (hasInputInvalid(inputElements)) {
     buttonElement.disabled = true;
-  } else {
-    buttonElement.disabled = false;
+    return;
   }
-};
+
+  buttonElement.disabled = false;
+}
 
 //grabs individual input elements from forms and puts them into an array. also grabs the submit button
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
-  const buttonElement = formElement.querySelector(".modal__button");
-  toggleButtonState(inputList, buttonElement);
+function setEventListeners(formElement, options) {
+  const { inputSelector } = options;
+  const { submitButtonSelector } = options;
+  const inputElements = Array.from(formElement.querySelectorAll(inputSelector));
+  const submitButton = formElement.querySelector(submitButtonSelector);
 
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", (e) => {
+      checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputElements, submitButton);
     });
   });
-};
+}
 
 //this function grabs the forms from the page and puts them into an array for processing
-function enableValidation() {
-  const formList = Array.from(document.querySelectorAll(".modal__form"));
+function enableValidation(options) {
+  const formList = Array.from(document.querySelectorAll(options.formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
-    setEventListeners(formElement);
+    setEventListeners(formElement, options);
   });
 }
 
-enableValidation();
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "form__button_disabled",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+};
+
+enableValidation(config);
